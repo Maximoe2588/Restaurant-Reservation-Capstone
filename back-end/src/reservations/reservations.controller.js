4cc37d8e87ade241ff4eb3230a5f0c7fef31a3fb
Maximoe2588 <70680274+Maximoe2588@users.noreply.github.com>
2023-03-27 20:11:52 -0500

    Update service/controller reservations

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,10 +1,36 @@
-/**
- * List handler for reservation resources
- */
-async function list(req, res) {
-  res.json({
-    data: [],
+const service = require("./reservations.service");
+
+async function reservationExists(req, res, next) {
+  const { reservationId } = req.params;
+  const reservation = await service.read(reservationId);
+
+  if (reservation) {
+    res.locals.reservation = reservation;
+    return next();
+  }
+  next({
+    status: 404,
+    message: `Reservation with id: ${reservationId} was not found`,
   });
+}
+
+async function list(req, res) {
+  try {
+    const reservations = await knex('reservations');
+    res.json({ data: reservations });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+async function read(req, res) {
+  const { reservation } = res.locals;
+  res.json({ data: reservation });
+}
+
+async function create(req, res) {
+  const reservation = await service.create(req.body.data);
+  res.status(201).json({ data: reservation });
 }
 
 module.exports = {
--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,5 +1,20 @@
 const knex = require("../db/connection");
 
+function read(id) {
+        return knex("reservations")
+            .select("*")
+            .where({ reservation_id: Number(id) })
+            .then((result) => result[0]);
+    }
+
+function create(reservation) {
+        return knex("reservations")
+            .insert(reservation)
+            .returning("*")
+            .then((result) => result[0]);
+        }
+
+
 module.exports = {
     
 }
