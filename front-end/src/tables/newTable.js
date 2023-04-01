9e8859ac06a4e715191404e059237f953e1afeb6
Maximoe2588 <70680274+Maximoe2588@users.noreply.github.com>
2023-04-01 16:13:22 -0500

    add tables seed

---
+++ front-end/src/tables/newTable.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import ErrorAlert from "../../layout/ErrorAlert";
+
+function NewTable() {
+    const [tablesError, setTablesError] = useState(null);
+    const history = useHistory();
+
+}
+
+export default NewTable;
