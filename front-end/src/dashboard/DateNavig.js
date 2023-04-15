1f62ff83976d6f8f84916c9bcf2781e168929066
Maximoe2588 <70680274+Maximoe2588@users.noreply.github.com>
2023-04-15 16:46:55 -0500

    update date navig

--- front-end/src/dashboard/DateNavig.js
+++ front-end/src/dashboard/DateNavig.js
@@ -8,27 +8,35 @@ function DateNavigation({ date }) {
   return (
     <>
       <h5 className="text-center mt-3 text-monospace">Date Navigation</h5>
-      <nav className="date-navigation">
+      <nav className="nav mb-1 bg-light w-75 mx-auto justify-content-center text-center"
+        aria-label="Change date">
         <Link
-          className="previous-link"
+          className="nav-link border border-left-0 border-secondary bg-white"
           to={`/dashboard?date=${previous(date)}`}
           aria-label="Previous"
         >
+        <span aria-hidden="true" className="font-size-12 font-weight-bold">
+            &laquo;{" "}
+        </span>
           <span> Previous </span>
         </Link>
         <Link
-          className="today-link"
+          className="nav-link border border-top-0 border-primary mx-3 px-2 bg-white"
           to={`/dashboard?date=${today()}`}
           aria-label="Today"
         >
           <span> Today </span>
         </Link>
         <Link
-          className="next-link"
+          className="nav-link border border-right-0 border-secondary bg-white"
           to={`/dashboard?date=${next(date)}`}
           aria-label="Next"
         >
           <span> Next </span>
+          <span aria-hidden="true" className="font-weight-bold">
+            {" "}
+            &raquo;
+          </span>
         </Link>
       </nav>
     </>
